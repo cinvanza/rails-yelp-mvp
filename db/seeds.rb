a95f075d4dd4b0c4da9ba6f9c38687a991409bf3
cintia vanesa zavi <cintia_zavi@hotmail.com>
2024-05-21 21:57:08 -0300

    seed ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,26 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+pizza_east = {name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "31123456789", category: "italian"}
+dishoom = {name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: "31123456789", category: "italian"}
+capri = {name: "Capri", address: "7 Boundary St, London E2 7JE", phone_number: "31123456789", category: "italian"}
+lucia = {name: "Lucia", address: "7 Boundary St, London E2 7JE", phone_number: "31123456789", category: "italian"}
+sopelsa = {name: "Sopelsa", address: "7 Boundary St, London E2 7JE", phone_number: "31123456789", category: "italian"}
+
+[dishoom, pizza_east, capri, lucia, sopelsa].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
+
+Restaurant.create(
+  name: "hola",
+  address: "hola2",
+  phone_number: "31123456789",
+  category: "japanese"
+)
